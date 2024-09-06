5fa5e898f453cafe337724bab7da359ad31cdab0
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-06 15:10:55 +0100

    add webhook

--- middleware/thirdPartymiddleware.js
+++ middleware/thirdPartymiddleware.js
@@ -1,9 +1,11 @@
 exports.thirdPartymiddleware = async (req, res, next) => {
-    const requestIP = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    
-    if (requestIP !== "::ffff:127.0.0.1") {
-        return res.status(403).json({ message: "Banned IP Address" });
+    if (req.url === '/webhooks/woocommerce') {
+      return next(); // Bypass IP check for webhook endpoint
     }
-
+  
+    const requestIP = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (requestIP !== "::ffff:127.0.0.1") {
+      return res.status(403).json({ message: "Banned IP Address" });
+    }
     next();
-}
+  }
