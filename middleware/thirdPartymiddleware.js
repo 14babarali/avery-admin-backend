f156bb2a9b00c1177110fb374477c6c81a062100
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-06 10:58:47 +0100

    update thirdPartymiddleware

--- middleware/thirdPartymiddleware.js
+++ middleware/thirdPartymiddleware.js
@@ -1,9 +1,10 @@
 exports.thirdPartymiddleware = async (req, res, next) => {
     const requestIP = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    
-    if (requestIP !== "::ffff:127.0.0.1") {
-        return res.status(403).json({ message: "Banned IP Address" });
+    const origin = req.headers.origin;
+  
+    if (origin === 'http://localhost:3002' || requestIP === "::ffff:127.0.0.1") {
+      next();
+    } else {
+      return res.status(403).json({ message: "Banned IP Address" });
     }
-
-    next();
-}
+  }
